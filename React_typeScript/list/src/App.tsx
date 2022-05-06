c48ed156498a2f4c7de1b0c4fb56f80507a236a8
FabioDiasRC <fabior.carvalho@hotmail.com>
2022-05-05 23:13:02 -0300

    add o começo do todo list

---
+++ React_typeScript/list/src/App.tsx
@@ -0,0 +1,16 @@
+import * as C from './App.styles'
+import './App.css' //reset css
+
+const App = () => {
+  return (
+    <>
+      <C.Container>
+        <C.Area>
+          <C.Header><h1>Teste</h1></C.Header>
+        </C.Area>
+      </C.Container>
+    </>
+  )
+}
+
+export default App;
